f1e1150326104f7e95a1b7015c087df057588efe
zarriei <75182441+zarriei@users.noreply.github.com>
2021-04-18 16:58:12 -0400

    added models

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+//! require("./seeders/seed"); // Either npm run seed or keep this in here uncommented and it will run the seed
+
+const PORT = process.env.PORT || 3005;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+//require(apiRoute)(app);
+require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
